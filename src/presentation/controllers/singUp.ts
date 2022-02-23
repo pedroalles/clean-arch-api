64d74a42f488c36796b4e0f8dd7fabc9cd4dade3
Pedro Alles <pedroalles@gmail.com>
2022-02-23 06:09:41 -0300

    feat: ensure SingUpController returns 400 and an error if invalid email is provided

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,9 +1,13 @@
-import { MissingParamError } from '../errors/missing-param-error'
 import { BadRequest } from '../helpers/http-helper'
+import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
+import { IEmailValidator } from '../protocols/email-validator'
 
 export class SingUpController implements Controller {
+  constructor (private readonly emailValidator: IEmailValidator) {}
+
   handle (httpRequest: IHttpRequest): IHttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -11,6 +15,11 @@ export class SingUpController implements Controller {
       if (!httpRequest.body[field]) {
         return BadRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return BadRequest(new InvalidParamError('email'))
     }
 
     return { statusCode: 200, body: {} }
