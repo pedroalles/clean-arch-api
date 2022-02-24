1fa62ac578cd30973618e913cd1aff425f29b2e3
pedroalles <pedroalles@gmail.com>
2022-02-24 01:17:26 -0300

    feat: ensure SingUpController returns 400 if password confirmation fails

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -11,6 +11,9 @@ export class SingUpController implements Controller {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) return badRequest(new InvalidParamError('email'))
       return { statusCode: 200, body: {} }
