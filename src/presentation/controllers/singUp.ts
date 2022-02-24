4c0584a1bb28725d16c02c187d973753169bf212
pedroalles <pedroalles@gmail.com>
2022-02-24 00:21:04 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/singUp.ts
+++ src/presentation/controllers/singUp.ts
@@ -1,10 +1,9 @@
-import { BadRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { IEmailValidator } from '../protocols/email-validator'
-import { ServerError } from '../errors/server-error'
 
 export class SingUpController implements Controller {
   constructor (private readonly emailValidator: IEmailValidator) {}
@@ -13,13 +12,13 @@ export class SingUpController implements Controller {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) return BadRequest(new MissingParamError(field))
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
-      if (!isValid) return BadRequest(new InvalidParamError('email'))
+      if (!isValid) return badRequest(new InvalidParamError('email'))
       return { statusCode: 200, body: {} }
     } catch (error) {
-      return { statusCode: 500, body: new ServerError() }
+      return serverError()
     }
   }
 }
