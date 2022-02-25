db0aec8177f0ba54f3da836a5049355e8f853f76
Pedro Alles <pedroalles@gmail.com>
2022-02-25 02:01:15 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -23,6 +23,7 @@ export class SingUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
