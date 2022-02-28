51700b274d113f96c6188c5bf17bd1e97d59cfab
pedroalles <pedroalles@gmail.com>
2022-02-28 04:40:05 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,9 @@
+import { IController, IHttpRequest, IHttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements IController {
+  constructor (private readonly controller: IController) {}
+  async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,9 +2,11 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongodbRespository } from '../../infra/db/mongodb/account-repository/account'
 import { SingUpController } from '../../presentation/controllers/singup/singup'
+import { IController } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSingUpController = (): SingUpController => {
+export const makeSingUpController = (): IController => {
   const salt = 12
 
   const emailValidatorAdapter =
@@ -22,5 +24,5 @@ export const makeSingUpController = (): SingUpController => {
   const singUpController =
     new SingUpController(emailValidatorAdapter, dbAddAccount)
 
-  return singUpController
+  return new LogControllerDecorator(singUpController)
 }
