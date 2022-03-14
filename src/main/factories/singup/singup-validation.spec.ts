2bb68c14057f1199d8dc2d0d62a237dade043946
pedroalles <pedroalles@gmail.com>
2022-03-14 02:21:34 -0300

    refactor: group validation imports in a single file

--- src/main/factories/singup/singup-validation.spec.ts
+++ src/main/factories/singup/singup-validation.spec.ts
@@ -1,7 +1,4 @@
-import { CompareFieldValidation } from '../../../presentation/helpers/validators/compare-field-validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { CompareFieldValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
 import { IEmailValidator } from '../../../presentation/protocols'
 import { makeSingUpValidation } from './signup-validation'
 
